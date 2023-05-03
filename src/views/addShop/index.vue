b32f60ffbf0fb649790aa3b3713540447b79177d
瞿玲 <quling@dist.com.cn>
2023-05-03 21:57:14 +0800

    首次提交

---
+++ src/views/addShop/index.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: quling
+ * @Date: 2023-05-03 21:44:47
+ * @LastEditors: quling
+ * @LastEditTime: 2023-05-03 21:50:36
+ * @Description: 新增门店
+ * @FilePath: \vue-admin-template\src\views\addShop\index.vue
+-->
+<template>
+  <div class="add-shop">新增门店</div>
+</template>
+
+<script>
+export default {
+  name: "AddShop",
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.add-shop {
+  padding: 15px;
+  height: 100%;
+}
+</style>
---
+++ src/views/portal/index.vue
@@ -0,0 +1,271 @@
+<!--
+ * @Author: quling
+ * @Date: 2023-04-27 22:44:28
+ * @LastEditors: quling
+ * @LastEditTime: 2023-05-03 21:49:44
+ * @Description: 首页
+ * @FilePath: \vue-admin-template\src\views\portal\index.vue
+-->
+<template>
+  <div class="portal-container">
+    <!-- 操作按钮 -->
+    <div class="operation">
+      <el-button type="primary" size="medium" @click="handleShopAdd">
+        新增门店
+      </el-button>
+      <el-button
+        type="danger"
+        size="medium"
+        :loading="delLoading"
+        :autofocus="false"
+        plain
+        @click="handleShopDel"
+      >
+        删除按钮
+      </el-button>
+    </div>
+    <!-- 表格 -->
+    <div class="table-container">
+      <el-table
+        ref="Table"
+        :data="tableData"
+        height="100%"
+        @selection-change="handleSelectionChange"
+        @row-click="handleRowClick"
+      >
+        <el-table-column type="selection" width="55"> </el-table-column>
+        <el-table-column fixed prop="date" label="日期"> </el-table-column>
+        <el-table-column prop="name" label="姓名"> </el-table-column>
+        <el-table-column prop="province" label="省份"> </el-table-column>
+        <el-table-column prop="city" label="市区"> </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+        <el-table-column prop="zip" label="邮编"> </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Portal",
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+      multipleSelection: [], // 表格选中项
+      delLoading: false,
+    };
+  },
+  methods: {
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    // 点击表格行
+    handleRowClick(row) {
+      console.log(row);
+      this.$refs.Table.toggleRowSelection(row);
+    },
+    // 按钮失焦
+    dissolveFocus(event) {
+      let target = event.target;
+      if (target.nodeName === "SPAN") {
+        target = event.target.parentNode;
+      }
+      target.blur();
+    },
+    // 增加门店
+    handleShopAdd(event) {
+      this.dissolveFocus(event);
+
+      this.$router.push({ name: "AddShop" });
+    },
+    // 删除门店
+    handleShopDel(event) {
+      this.dissolveFocus(event);
+      if (this.multipleSelection.length === 0) {
+        this.$message({
+          message: "未选中需门店",
+          type: "warning",
+        });
+        return;
+      }
+      this.$confirm("此操作将永久删除选中门店, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.portal-container {
+  padding: 15px;
+  display: grid;
+  height: 100%;
+  grid-template-rows: 50px calc(100% - 50px);
+  .operation {
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    width: 100%;
+  }
+  .table-container {
+    width: 100%;
+    overflow: hidden;
+  }
+}
+</style>
